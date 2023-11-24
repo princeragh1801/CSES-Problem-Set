db8b0af77f59fd0feb341eea8bbccf5e38cbbfc4
Prince <princeraghuwanshi1801@gmail.com>
2023-11-24 11:38:14 +0530

    adding solutions of introductory problems

---
+++ Introductory Problems/permutations.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i, a, b) for(ll int i = a; i < b; i++)
+#define mod 1000000007
+#define beg(x) x.begin(), x.end()
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<endl;
+#else
+#define debug(x)
+#endif
+ 
+ 
+void _print(int a){
+    cerr<<a;
+}
+ 
+void _print(float a){
+    cerr<<a;
+}
+ 
+void _print(double a){
+    cerr<<a;
+}
+ 
+void _print(ll a){
+    cerr<<a;
+}
+ 
+void _print(char a){
+    cerr<<a;
+}
+ 
+void _print(bool a){
+    cerr<<a;
+}
+ 
+void _print(string a){
+    cerr<<a;
+}
+ 
+template<class T> void _print(vector<T> v){
+    cerr<<"[ ";
+    for(T it : v){
+        _print(it);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+ 
+template<class T> void _print(set<T> s){
+    cerr<<"[ ";
+    for(T it : s){
+        _print(it);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+ 
+ 
+template<class T> void _print(unordered_set<T> s){
+    cerr<<"[ ";
+    for(T it : s){
+        _print(it);
+        cerr<<" ";
+    }
+    cerr<<"]";
+}
+ 
+/*********************** prince1801 ************************/
+ 
+void _solve(){
+  ll n;
+  cin>>n;
+  if(n == 1){
+    cout<<1<<endl;
+  }
+  else if(n <= 3){
+    cout<<"NO SOLUTION";
+  }
+  
+  else{
+    int i = n-1;
+    while(i > 0){
+        cout<<i<<" ";
+        i-=2;
+    }
+    i = n;
+    while(i > 0){
+        cout<<i<<" ";
+        i-=2;
+    }
+  }
+  cout<<endl;
+}
+ 
+int main()
+{
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("error.txt", "w", stderr);
+   freopen("output.txt", "w", stdout);
+   #endif
+  
+  _solve();
+  return 0;
+}
---
+++ Introductory Problems/two_knights.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i, a, b) for(int i = a; i < b; i++)
+#define mod 1000000007
+#define beg(x) x.begin(), x.end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x);cerr<<endl;
+#else
+#define debug(x)
+#endif
+void _print(int a){cerr<<a;}
+void _print(float a){cerr<<a;}
+void _print(double a){cerr<<a;}
+void _print(ll a){cerr<<a;}
+void _print(char a){cerr<<a;}
+void _print(bool a){cerr<<a;}
+void _print(string a){cerr<<a;}
+template<class T> void _print(vector<T> v){cerr<<"[ ";for(T it : v){_print(it);cerr<<" ";}cerr<<"]";}
+template<class T> void _print(set<T> s){cerr<<"[ ";for(T it : s){_print(it);cerr<<" ";}cerr<<"]";}
+template<class T> void _print(unordered_set<T> s){cerr<<"[ ";for(T it:s){_print(it);cerr<<" ";}cerr<<"]";}
+ll gcd(ll a, ll b){return b ? gcd(b, a%b) : a;}
+ll lcm(ll a, ll b){ll g = gcd(a,b);return (a*b)/g;}
+ll extendedGcd(ll a, ll b, ll x, ll y){if(a == 0){x = 0;y = 1;return b;}int x1, y1;return extendedGcd(b, a%b, x1, y1);}
+/*********************** prince1801 ************************/
+
+void _solve(){
+    int n;
+    cin>>n;
+    cout<<0<<nl;
+    for(int k = 2; k <= n; k++){
+        // total number of places in chessborad k*k
+        ll num = k*k;
+
+        // total number of ways we can place knights on chessboard
+        ll totalNumberofWays = (num*(num-1))/2;
+        ll numberOfWaysKnightsAttack = 4*(k-1)*(k-2);
+        ll numberOfWays = totalNumberofWays-numberOfWaysKnightsAttack;
+        cout<<numberOfWays<<nl;
+    }
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("error.txt", "w", stderr);
+  freopen("output.txt", "w", stdout);
+  #endif
+  int test_case_ = 1;
+//   cin>>test_case_;
+  while(test_case_--){ _solve(); }
+  return 0;
+}
